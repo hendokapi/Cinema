82a74ac28e493227961c2bf956a54bb888938935
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2024-02-14 21:32:40 +0400

    Add project files.

---
+++ Cinema/Index.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Cinema.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cinema</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Cinema camelCase</h1>
+
+        <div>
+            Sala Nord: 
+            <asp:Label ID="LblNord" runat="server" Text="Label"></asp:Label>
+            <br />
+            Sala Est:
+            <asp:Label ID="LblEst" runat="server" Text="Label"></asp:Label>
+            <br />
+            Sala Sud: 
+            <asp:Label ID="LblSud" runat="server" Text="Label"></asp:Label>
+        </div>
+
+        <br />
+
+        <div>
+            Nome: <asp:TextBox ID="TxtNome" runat="server"></asp:TextBox>
+            <br /><br />
+            Cognome: <asp:TextBox ID="TxtCognome" runat="server"></asp:TextBox>
+            <br /><br />
+            Sala:
+            <asp:RadioButtonList ID="LstSala" runat="server">
+                <asp:ListItem Value="nord">Nord</asp:ListItem>
+                <asp:ListItem Value="est">Est</asp:ListItem>
+                <asp:ListItem Value="sud">Sud</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+            <asp:CheckBox ID="ChkRidotto" runat="server" Text="Ridotto" />
+            <br /><br />
+            <asp:Button ID="Button1" runat="server" Text="Salva" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Cinema/Index.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cinema
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        private static string connString = ConfigurationManager.ConnectionStrings["DbCinemaConnectionString"].ToString();
+        private SqlConnection conn = new SqlConnection(connString);
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            TxtNome.Text = "";
+            TxtCognome.Text = "";
+            LstSala.SelectedIndex = 0;
+            ChkRidotto.Checked = false;
+
+            conn.Open();
+
+            SqlCommand commandCountNord = new SqlCommand("SELECT COUNT(*) FROM Biglietti WHERE Sala='nord'", conn);
+            string countNord = commandCountNord.ExecuteScalar().ToString();
+            SqlCommand commandCountNordReduced = new SqlCommand("SELECT COUNT(*) FROM Biglietti WHERE Sala='nord' AND Ridotto=1", conn);
+            string countNordReduced = commandCountNordReduced.ExecuteScalar().ToString();
+            LblNord.Text = $"Totali: {countNord}, ridotti: {countNordReduced}";
+
+            SqlCommand commandCountSud = new SqlCommand("SELECT COUNT(*) FROM Biglietti WHERE Sala='sud'", conn);
+            string countSud = commandCountSud.ExecuteScalar().ToString();
+            SqlCommand commandCountSudReduced = new SqlCommand("SELECT COUNT(*) FROM Biglietti WHERE Sala='sud' AND Ridotto=1", conn);
+            string countSudReduced = commandCountSudReduced.ExecuteScalar().ToString();
+            LblSud.Text = $"Totali: {countSud}, ridotti: {countSudReduced}";
+
+            SqlCommand commandCountEst = new SqlCommand("SELECT COUNT(*) FROM Biglietti WHERE Sala='est'", conn);
+            string countEst = commandCountEst.ExecuteScalar().ToString();
+            SqlCommand commandCountEstReduced = new SqlCommand("SELECT COUNT(*) FROM Biglietti WHERE Sala='est' AND Ridotto=1", conn);
+            string countEstReduced = commandCountEstReduced.ExecuteScalar().ToString();
+            LblEst.Text = $"Totali: {countEst}, ridotti: {countEstReduced}";
+
+            conn.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand saveTicket = new SqlCommand("" +
+                    $"INSERT INTO Biglietti (Nome, Cognome, Sala, Ridotto) VALUES " +
+                    $"('{TxtNome.Text}', '{TxtCognome.Text}', '{LstSala.SelectedItem.Value}', {Convert.ToInt32(ChkRidotto.Checked)})" +
+                    "", conn);
+
+                int affectedRows = saveTicket.ExecuteNonQuery();
+
+                if (affectedRows == 0)
+                {
+                    Response.Write("Dati non inseriti");
+                }
+            }
+            catch
+            {
+                Response.Write("qualcosa non va");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
